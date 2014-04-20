551ba9284dd39d4af27a26f8516fe9406d7d6b06
Quynh Doan <quynhddoan@gmail.com>
2014-04-19 21:04:05 -0700

    Added email validations

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
-describe ApplicationHelpder do
+describe ApplicationHelper do
     describe "full_title" do
         it "should include the page title" do
-            expect(full_title("foo")).to_match(/foo/)
+            expect(full_title("foo")).to match(/foo/)
         end
 
         it "should include the base title" do
-            expect(full_title("foo")).to_match(/^Ruby on Rails Tutorial Sample App/)
+            expect(full_title("foo")).to match(/^Ruby on Rails Tutorial Sample App/)
         end
 
         it "should not include a bar for the home page" do
